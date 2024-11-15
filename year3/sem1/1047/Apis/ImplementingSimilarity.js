277d87fa21378beb3972d921ebc4738e1c69216e
SomethingOnline <jack.egan45@mail.dcu.ie>
2024-11-15 18:31:31 +0000

    added files

--- year3/sem1/1047/Apis/ImplementingSimilarity.js
+++ year3/sem1/1047/Apis/ImplementingSimilarity.js
@@ -3,7 +3,7 @@
 //		"https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
 //		{
 //			headers: {
-//				Authorization: "Bearer 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",
+//				Authorization: "Bearer ",
 //				"Content-Type": "application/json",
 //			},
 //			method: "POST",
@@ -34,7 +34,7 @@
 //			"https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
 //			{
 //				headers: {
-//					Authorization: "Bearer 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",
+//					Authorization: "Bearer ",
 //					"Content-Type": "application/json"
 //				},
 //				method: "POST",
